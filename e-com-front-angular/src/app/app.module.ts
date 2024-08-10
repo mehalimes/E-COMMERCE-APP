87f4860de13629f4d8b0cb0fce3c3c45c3611f6f
mehalimes <alymeshe05@gmail.com>
2024-08-10 15:24:56 +0300

    2 Products Added

--- e-com-front-angular/src/app/app.module.ts
+++ e-com-front-angular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from "./components/header/header.component";
 import { ContainerComponent } from './components/container/container.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent
+    ContainerComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ e-com-front-angular/src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.css',
+})
+export class ProductCardComponent {
+  @Input() productImage: string;
+  @Input() stockStatus: number;
+  @Input() productName: string;
+  @Input() productPrice: number;
+}
